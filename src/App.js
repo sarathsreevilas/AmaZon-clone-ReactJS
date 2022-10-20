15fd76770211ada792e234556f631239c64aaffb
smug <supersarath007@gmail.com>
2022-10-21 01:07:18 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    //BEM
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header/>
+            <h1>YOoo Lets Check Out...</h1>
+          </Route>
+          <Route path="/">
+            <Header/>
+            <Home/>
+          </Route>
+        </Switch>
+          
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBoiKEOe8yA5knU_iQWSOTVY7fsFPieT9E",
+    authDomain: "challenge-62ffe.firebaseapp.com",
+    projectId: "challenge-62ffe",
+    storageBucket: "challenge-62ffe.appspot.com",
+    messagingSenderId: "22675409144",
+    appId: "1:22675409144:web:0ca951292eabd590f9d30d",
+    measurementId: "G-1S3ZP52DWK"
+  };
